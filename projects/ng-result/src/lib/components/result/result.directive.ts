a7fda953ff885d39acfdc651ed073596acf1b285
Lonli-Lokli <lonli.lokli@gmail.com>
2022-11-29 18:16:53 +0000

    Add failure context to the result component

--- projects/ng-result/src/lib/components/result/result.directive.ts
+++ projects/ng-result/src/lib/components/result/result.directive.ts
@@ -1,21 +1,21 @@
 import { Directive, TemplateRef } from '@angular/core';
 import { ResultComponent } from './result.component';
 
-@Directive({ selector: '[ll-initial]' })
+@Directive({ selector: '[llInitial]' })
 export class ResultInitialDirective<E, D> {
   public constructor(public templateRef: TemplateRef<any>, result: ResultComponent<D>) {
     result.initialRef = templateRef;
   }
 }
 
-@Directive({ selector: '[ll-pending]' })
+@Directive({ selector: '[llPending]' })
 export class ResultPendingDirective<E, D> {
   public constructor(public templateRef: TemplateRef<any>, result: ResultComponent<D>) {
     result.pendingRef = templateRef;
   }
 }
 
-@Directive({ selector: '[ll-failure]' })
+@Directive({ selector: '[llFailure]' })
 export class ResultFailureDirective<E, D> {
   public constructor(public templateRef: TemplateRef<any>, result: ResultComponent<D>) {
     result.failureRef = templateRef;
